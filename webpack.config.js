2257b0485273f4eecdfb419fa815706367f35082
admin <tech@friends.casino>
2024-10-14 09:16:12 +0200

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,12 @@ module.exports = {
     output: {
         filename: 'iframeCommunication.min.js',
         path: path.resolve(__dirname, 'dist'),
-        library: 'IframeCommunication',
-        libraryTarget: 'umd',  // Universal Module Definition to support CommonJS, AMD, and browser globals
-        globalObject: 'this',  // Ensures the library works in Node and the browser
+        library: 'IframeCommunication', // Expose the class as a global variable
+        libraryTarget: 'window', // Attach it to the window object
     },
     mode: 'production',
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin()],  // Use Terser for minimization
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Apply loader to JavaScript files
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',  // Optional: You can add Babel for ES6+ transpiling
-                    options: {
-                        presets: ['@babel/preset-env'],  // Preset for transpiling ES6+
-                    },
-                },
-            },
-        ],
+        minimizer: [new TerserPlugin()], // TerserPlugin for minification
     },
 };
